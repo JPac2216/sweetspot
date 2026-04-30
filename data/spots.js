1453959a7b0529482e596c1a45cdbe71aff12c3f
Sameer-Sethuram <ssethura@stevens.edu>
2026-04-30 19:15:08 -0400

    Completed the following functions: addToSchedule, deleteFromSchedule, addSpotRating, deleteSpotRating. Completed all with full input validation, updating and querying of the database, as well as returning the object that is in the database after changes are made (using returnDocument: "after")

--- data/spots.js
+++ data/spots.js
@@ -37,3 +37,97 @@ export const createSpot = async (
 
     return { "spotCreated": true };
 };
+
+
+export const addSpotRating = async (
+    spotId,
+    userId,
+    username,
+    comment,
+    rating
+) => {
+    if (!spotId || typeof spotId !== "string" || !userId || typeof userId !== "string" || !username || typeof username !== "string" || !comment || typeof comment !== "string" || !rating || typeof rating !== "number") throw "addSpotRating: all parameters must be supplied to this function.";
+    spotId = spotId.trim();
+    userId = userId.trim();
+    username = username.trim();
+    comment = comment.trim();
+    if (!ObjectId.isValid(spotId) || !ObjectId.isValid(userId) || !username || !comment || Number.isNaN(rating) || !Number.isFinite(rating) || rating > 5 || rating < 1) throw "addSpotRating: userId parameter must be an ObjectId, username and comment parameters must be strings, and rating parameter must be a valid number between 1 and 5.";
+
+    const usersCollection = await users();
+    const findUser = await usersCollection.findOneAndUpdate({ _id: new ObjectId(userId), username });
+    if (!findUser) throw "addSpotRating: user with that userId and username combination is not stored within usersCollection.";
+
+    const spotsCollection = await spots();
+    
+    const findSpot = await spotsCollection.findOne({ _id: new ObjectId(spotId) });
+
+    let count = findSpot.sweetspotRating.count + 1;
+    let average = ((findSpot.sweetspotRating.count * findSpot.sweetspotRating.average) + rating) / count;
+
+    const findAndUpdateSpot = await spotsCollection.findOneAndUpdate(
+        { _id: new ObjectId(spotId)}, 
+        {
+            $set: {
+                sweetspotRating: {
+                    average,
+                    count
+                }
+            },
+            $push: {
+                comments: {
+                    _id: new ObjectId(),
+                    userId,
+                    username,
+                    comment,
+                    "createdAt": helper.getCurrentDate()
+                }
+            }
+        },
+        {
+            returnDocument: "after"
+        }
+    );
+    if (!findAndUpdateSpot) throw "addSpotRating: comment could not be added to the spot.";
+
+    return findAndUpdateSpot;
+};
+
+export const deleteSpotRating = async (
+    spotId,
+    commentId
+) => {
+    if (!spotId || typeof spotId !== "string" || !commentId || typeof commentId !== "string") throw "deleteSpotRating: all parameters must be supplied.";
+    spotId = spotId.trim();
+    commentId = commentId.trim();
+    if (!ObjectId.isValid(spotId) || !ObjectId.isValid(commentId)) throw "deleteSpotRating: spotId and commentId parameters must be valid ObjectIds.";
+
+    const spotsCollection = await spots();
+    
+    const findSpot = await spotsCollection.findOne({ _id: new ObjectId(spotId) });
+
+    let count = findSpot.sweetspotRating.count - 1;
+    let average = ((findSpot.sweetspotRating.count * findSpot.sweetspotRating.average) - rating) / count;
+
+    const findAndUpdateSpot = await spotsCollection.findOneAndUpdate(
+        { _id: new ObjectId(spotId)}, 
+        {
+            $set: {
+                sweetspotRating: {
+                    average,
+                    count
+                }
+            },
+            $pull: {
+                comments: {
+                    _id: commentId,
+                }
+            }
+        },
+        {
+            returnDocument: "after"
+        }
+    );
+    if (!findAndUpdateSpot) throw "deleteSpotRating: comment could not be deleted from the spot.";
+
+    return findAndUpdateSpot;
+};
