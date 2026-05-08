0decbb011299c3fbb2c863cd12bb27c503d03e6b
Sarah Simbulan <ssimbulan725@gmail.com>
2026-05-08 02:24:25 -0400

    adjusted handlebars to the new route syntax that everyone else pushed + added ids to the forms and inputs (matches the name and class fields) + routes for user functions and took out the mongocollections call and replaced with existing data function call + added id and username fields into newuser object and session cookie + adjusted userValidation helper function so that username isn't flagged when updating user profile amongst other subtle validation changes when the user is logged in

--- routes/auth.js
+++ routes/auth.js
@@ -57,9 +57,11 @@ router
     }
 
     req.session.member = {
+      _id: user._id,
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
+      username: user.username,
       membershipLevel: user.membershipLevel,
       datePoints: user.datePoints,
       primaryLocation: user.primaryLocation,
@@ -86,26 +88,32 @@ router
     let firstName = req.body.firstName;
     let lastName = req.body.lastName;
     let email = req.body.email;
+    let username = req.body.username;
     let password = req.body.password;
     let confirmPassword = req.body.confirmPassword;
     let gender = req.body.gender;
     let primaryLocation = req.body.primaryLocation;
     let secondaryLocation = req.body.secondaryLocation;
 
-    if (!firstName || !lastName || !email || !password || !gender || !primaryLocation || !confirmPassword){
+    if (!firstName || !lastName || !email || !username || !password || !gender || !primaryLocation || !confirmPassword){
       return res.status(400).render('register', {error: 'All fields must be supplied to create a user.', title: 'Error: Missing Fields'});
     }
-   
+
+    const username_regex = /^[a-zA-Z0-9_]{3,20}$/;
+    if (!username_regex.test(username.trim())) {
+      return res.status(400).render('register', {error: 'Username must be 3-20 characters and only contain letters, numbers, and underscores.', title: 'Error: Invalid Username'});
+    }
+
     //error checking
     let validated = null;
     try{
-      validated = await helpers.validateUser(firstName, lastName, email, password, confirmPassword, gender, primaryLocation, secondaryLocation);
+      validated = await helpers.validateUser(firstName, lastName, email, username, password, confirmPassword, gender, primaryLocation, secondaryLocation);
     } catch(e){
       return res.status(400).render('register', {error: e, title: 'Error: Invalid Input'});
     }
 
     try{
-      let newUser = await createUser(validated.firstName, validated.lastName, validated.email, validated.password, validated.gender, validated.primaryLocation, validated.secondaryLocation);
+      let newUser = await createUser(validated.firstName, validated.lastName, validated.email, validated.username, validated.password, validated.gender, validated.primaryLocation, validated.secondaryLocation);
       if(newUser.userCreated !== true || !newUser){
         return res.status(500).render('register', {error: 'Failed to create user', title: 'Error: Internal Server Error'});
       }else{
